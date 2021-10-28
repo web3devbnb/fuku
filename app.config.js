3b9e2816e9dc25f6c658413deb8d839230037d63
galaxy126 <galaxy85126@gmail.com>
2021-10-28 15:37:57 +0800

    fix: config

--- app.config.js
+++ app.config.js
@@ -11,7 +11,7 @@ const appConfig = {
         // list of providers. if one of them is unavailable, another is randomly picked
         providers: [
             {
-                http: 'http://18.117.255.252/graphql', // http://127.0.0.1:16761/graphql
+                http: 'http://13.59.118.124/graphql', // http://127.0.0.1:16761/graphql
                 // for subscriptions
                 ws: '',
             },
